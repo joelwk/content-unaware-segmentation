0bd5abb50c75d6cc5e5e2f5ec18ac5c2d00c35f9
Joel <jw.konitzer@gmail.com>
2023-10-18 08:02:06 -0400

    updated load_data and added plots

--- pipeline/video2dataset.py
+++ pipeline/video2dataset.py
@@ -118,6 +118,7 @@ def main():
         original_video_df = pd.DataFrame(original_video_locs)
         keyframe_video_df['duration'] = keyframe_video_df['duration'].astype(float)
         original_video_df['duration'] = original_video_df['duration'].astype(float)
+
         keyframe_video_df.to_parquet(f'{directory}/keyframe_video_requirements.parquet', index=False)
         original_video_df.to_parquet(f'{directory}/original_video_requirements.parquet', index=False)
 
@@ -149,7 +150,7 @@ def main():
             print("STDOUT:", result.stdout)
 
     external_parquet_path = "./youtube.parquet"  # Replace with actual path or None
-    prepare_dataset_requirements(selected_config["directory"], external_parquet_path = external_parquet_path)
+    prepare_dataset_requirements(selected_config["directory"], external_parquet_path = None)
     run_video2dataset_with_yt_dlp(selected_config["directory"], selected_config["original_videos"])
     fix_codecs_in_directory(selected_config["original_videos"])
     segment_key_frames_in_directory(selected_config["original_videos"], selected_config["keyframe_videos"])
