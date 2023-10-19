71b791b981ed64f91e328d7d34e3148db2a62542
Joel <jw.konitzer@gmail.com>
2023-10-19 18:23:54 -0400

    updated webdataset and grouping functions

--- pipeline/move_and_group.py
+++ pipeline/move_and_group.py
@@ -3,7 +3,7 @@ import glob
 import shutil
 
 def move_and_group_files():
-    # Define source directories
+    # Define source directories for various categories of files
     src_dirs = {
         'originalvideos': './datasets/originalvideos',
         'keyframevideos': './datasets/keyframes',
@@ -13,26 +13,27 @@ def move_and_group_files():
         'keyframe_clip_embeddings': './keyframe_clip_embeddings/'
     }
     
-    # Define destination directory
+    # Define the destination directory where files will be moved to
     dest_dir = './completedatasets'
     os.makedirs(dest_dir, exist_ok=True)
     
-    # Initialize a dictionary to keep track of integer suffixes
+    # Initialize a dictionary to store integer suffixes for each category
     integer_suffixes = {}
     
+    # Loop through source directories and list files
     for category, src_directory in src_dirs.items():
         for file_path in glob.glob(f"{src_directory}/*"):
             basename = os.path.basename(file_path)
             
-            # Skip stats files
+            # Skip any files that are statistics files
             if basename.endswith("_stats"):
                 continue
             
-            # Extract integer suffix
+            # Extract integer suffix from file name
             integer_suffix = basename.split('.')[0]
             
+            # For some categories, there are nested files; process them
             if category in ['keyframes', 'keyframe_clips', 'keyframe_clip_embeddings']:
-                # For these categories, go one level deeper
                 for nested_file in glob.glob(f"{file_path}/*"):
                     if integer_suffix not in integer_suffixes:
                         integer_suffixes[integer_suffix] = []
@@ -42,6 +43,7 @@ def move_and_group_files():
                     integer_suffixes[integer_suffix] = []
                 integer_suffixes[integer_suffix].append((category, file_path))
                 
+    # Create destination directories and move files
     for integer_suffix, file_tuples in integer_suffixes.items():
         integer_dest_dir = os.path.join(dest_dir, integer_suffix)
         os.makedirs(integer_dest_dir, exist_ok=True)
@@ -50,9 +52,22 @@ def move_and_group_files():
             category_dest_dir = os.path.join(integer_dest_dir, category)
             os.makedirs(category_dest_dir, exist_ok=True)
             
+            # Move the file to the new location
             new_file_path = os.path.join(category_dest_dir, os.path.basename(file_path))
             shutil.move(file_path, new_file_path)
             print(f"Moved {file_path} to {new_file_path}")
+            
+    # Function to remove unwanted directories from the destination folder
+    def cleanup_unwanted_dirs(directory, unwanted_dirs):
+        # Loop through each unwanted directory and remove it
+        for unwanted_dir in unwanted_dirs:
+            path_to_remove = os.path.join(directory, unwanted_dir)
+            if os.path.exists(path_to_remove):
+                shutil.rmtree(path_to_remove)
+                print(f"Removed {path_to_remove}")
+
+    # Call the cleanup function to remove unwanted directories like '00000_stats' and '00000'
+    cleanup_unwanted_dirs('./completedatasets', ['00000_stats', '00000'])
 
 if __name__ == "__main__":
     move_and_group_files()
--- pipeline/save_to_webdataset.py
+++ pipeline/save_to_webdataset.py
@@ -1,37 +1,70 @@
 import os
 import glob
-import tarfile
 from webdataset import ShardWriter
+import numpy as np
+import json
+from io import BytesIO
+import ast
 
 def package_datasets_to_webdataset(root_folder, output_folder, shard_size=1e9):
+    # List all folders in the root directory and sort them
     dataset_folders = sorted(glob.glob(f"{root_folder}/*"))
+    
+    # Create a pattern for shard file naming
     pattern = os.path.join(output_folder, "completed_datasets-%06d.tar")
-
+    
     def recursive_add_files(folder_path, sample, parent_key):
+        # Loop through each file and folder in the directory
         for item in os.listdir(folder_path):
             item_path = os.path.join(folder_path, item)
             
+            # If it's a directory, recurse into it
             if os.path.isdir(item_path):
                 new_key = f"{parent_key}/{item}"
                 recursive_add_files(item_path, sample, new_key)
             else:
+                # Prepare a key to represent this data item
                 category = f"{parent_key}/{item}"
-                extension = os.path.splitext(item)[-1]
+                extension = os.path.splitext(item)[-1][1:]  # Strip the dot from the extension
                 
-                with open(item_path, 'rb') as f:
-                    sample[f"{category}{extension}"] = f.read()
+                # Explicitly check types and add assert statements
+                
+                # Handle .npy files (NumPy arrays)
+                if extension == 'npy':
+                    array = np.load(item_path)
+                    assert isinstance(array, np.ndarray)
+                    sample[f"{category}"] = array  
+                elif extension == 'json':
+                    # Handle JSON files
+                    with open(item_path, 'r') as f:
+                        json_data = json.load(f)
+                        assert isinstance(json_data, (list, dict))
+                        sample[f"{category}.json"] = json_data
+                else:
+                    # Handle other types of files
+                    with open(item_path, 'rb') as f:
+                        buffer = BytesIO(f.read())  # Read file as a byte stream and store in a BytesIO buffer
+                        assert isinstance(buffer, BytesIO)
+                        sample[f"{category}.{extension}"] = buffer.getvalue()  # Get the byte stream from the buffer
 
+    # Initialize ShardWriter with the naming pattern and maximum shard size
     with ShardWriter(pattern, maxsize=shard_size) as sink:
+        # Loop through each dataset folder
         for i, dataset_folder in enumerate(dataset_folders):
             sample = {}
-            sample['__key__'] = os.path.basename(dataset_folder)
             
+            # Use the folder name as a key for the entire sample
+            sample['__key__'] = os.path.basename(dataset_folder)
+            assert isinstance(sample['__key__'], str)
+            
+            # Populate the sample dictionary with data
             recursive_add_files(dataset_folder, sample, sample['__key__'])
             
+            # Write the sample into a shard
             sink.write(sample)
 
 if __name__ == '__main__':
-    root_folder = '/content/completedatasets'
-    output_folder = '/content/shards'
+    root_folder = './completedatasets'
+    output_folder = '.'
     os.makedirs(output_folder, exist_ok=True)
     package_datasets_to_webdataset(root_folder, output_folder)
